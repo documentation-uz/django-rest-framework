f2ac20eee58e2703c091790fd1f4d6e2bbb5a40f
asliddin750750 <asliddin750750@gmail.com>
2023-08-22 13:12:29 +0500

    Edit this page on GitHub link was added

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,6 +12,10 @@ export default defineConfig({
         search: {
             provider: 'local'
         },
+        editLink: {
+            pattern: 'https://github.com/documentation-uz/django-rest-framework/tree/main/src/pages/:path',
+            text: 'Edit this page on GitHub',
+        },
         // https://vitepress.dev/reference/default-theme-config
         nav: [
             {text: 'Home', link: '/'},
